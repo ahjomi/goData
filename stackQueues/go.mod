30f43c2684ad412abb0be8c85989d9e0a9ac9af5
jeomiah <ehdaorwn3192@gmail.com>
2022-05-19 19:15:48 +0900

    stack and queues created

---
+++ stackQueues/go.mod
@@ -0,0 +1,3 @@
+module github.com/ahjomi/stackQueues
+
+go 1.18
---
+++ stackQueues/main.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+// Stack represents stack that hold a sloce
+type Stack struct {
+	items []int
+}
+
+// Queue represents a queue that holds a slice
+type Queue struct {
+	items []int
+}
+
+// Push will add a value at the end
+func (s *Stack) Push(i int) {
+	s.items = append(s.items, i)
+}
+
+// Pop will remove a value at the end
+// and RETURNs the removed value
+func (s *Stack) Pop() int {
+	l := len(s.items) - 1
+	toRemove := s.items[l]
+	s.items = s.items[:l]
+	return toRemove
+}
+
+// Enqueue adds a value at the end
+func (q *Queue) Enqueue(i int) {
+	q.items = append(q.items, i)
+}
+
+// Dequeue removes a value at the front
+// and RETURNs the removed value
+func (q *Queue) Dequeue() int {
+	toRemove := q.items[0]
+	q.items = q.items[1:]
+	return toRemove
+}
+
+func main() {
+
+	myQueue := Queue{}
+	myStack := Stack{}
+	fmt.Println(myQueue)
+	myQueue.Enqueue(100)
+	myQueue.Enqueue(200)
+	myQueue.Enqueue(300)
+	fmt.Println(myQueue)
+	myQueue.Dequeue()
+	fmt.Println(myQueue)
+	fmt.Println(myStack)
+	myStack.Push(100)
+	myStack.Push(200)
+	myStack.Push(300)
+	fmt.Println(myStack)
+	myStack.Pop()
+	fmt.Println(myStack)
+
+}
